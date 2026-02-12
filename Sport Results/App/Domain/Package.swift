4254c5b8617eca6e7f91809c6e0dfbda4dbcaba9
Ctibor Šebák <sebakctibor@gmail.com>
2026-02-12 23:01:58 +0100

    Add match result related domain objects

---
+++ Sport Results/App/Domain/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 6.0
+
+import PackageDescription
+
+let package = Package(
+    name: "Domain",
+    platforms: [.iOS(.v18)],
+    products: [
+        .library(
+            name: "Domain",
+            targets: ["Domain"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "Domain"
+        ),
+    ]
+)
